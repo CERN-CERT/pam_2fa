50986444005e87823b4d625df2114ceccbb6532c
Orion Poplawski <orion@nwra.com>
2018-11-20 11:38:40 -0700

    Use openssh upstream's SSH_AUTH_INFO_0 if present

--- pam_ssh_user_auth.c
+++ pam_ssh_user_auth.c
@@ -49,7 +49,7 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t * pamh, int flags,
 
     ssh_user_auth = get_ssh_user_auth(pamh, debug);
     if (ssh_user_auth == NULL) {
-        /* There was no SSH_USER_AUTH in the environment, which can be caused by:
+        /* There was no SSH_AUTH_INFO_0 or SSH_USER_AUTH in the environment, which can be caused by:
          *  - This feature not being supported by the installed version of OpenSSH
          *  - No previously successful authentications
          *  Here, we will assume that we are in the latter case
@@ -58,7 +58,7 @@ PAM_EXTERN int pam_sm_authenticate(pam_handle_t * pamh, int flags,
     }
 
     /* We have no requirement on which authentication methods should be authorized
-     * As we have a non-empty SSH_USER_AUTH, accept the request
+     * As we have a non-empty SSH_AUTH_INFO_0 or SSH_USER_AUTH, accept the request
      * TODO: add a parameter containing the list of authorized auth methods.
      */
     return PAM_SUCCESS;
